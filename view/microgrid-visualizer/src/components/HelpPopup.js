b267f3eef3f1ddc45311a7ae2ba76f31c1d44de6
ViderLab <117027996+theviderlab@users.noreply.github.com>
2025-07-17 20:10:05 +0200

    fix help popup markdown rendering

--- view/microgrid-visualizer/src/components/HelpPopup.js
+++ view/microgrid-visualizer/src/components/HelpPopup.js
@@ -1,19 +1,15 @@
 import { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './HelpPopup.css';
 
 export default function HelpPopup({ onClose }) {
   const [content, setContent] = useState('');
-  const [Markdown, setMarkdown] = useState(null);
 
   useEffect(() => {
-    fetch('/USER_MANUAL.md')
+    fetch(`${process.env.PUBLIC_URL}/USER_MANUAL.md`)
       .then((res) => res.text())
       .then(setContent)
       .catch(() => setContent('Failed to load manual.'));
-
-    import('react-markdown')
-      .then((mod) => setMarkdown(() => mod.default))
-      .catch(() => setMarkdown(null));
   }, []);
 
   return (
@@ -23,7 +19,7 @@ export default function HelpPopup({ onClose }) {
           &times;
         </button>
         <div className="help-content">
-          {Markdown ? <Markdown>{content}</Markdown> : <pre>{content}</pre>}
+          <ReactMarkdown>{content}</ReactMarkdown>
         </div>
       </div>
     </div>
